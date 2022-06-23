2b536a3ae8243869d6e3bb53c20146af131c8cce
pro100kryto <pro100kryto.original@gmail.com>
2022-06-23 15:31:55 -0300

    Improvement Program node

--- Node/Program.cs
+++ Node/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using SimpleUdp;
 
@@ -7,128 +8,168 @@ namespace Node
 {
     class Program
     {
-        static string _Ip = null;
-        static int _Port = 0;
-        static UdpEndpoint _UdpEndpoint;
+        static UdpEndpoint _udpEndpoint;
 
         static void Main(string[] args)
         {
-            if (args == null || args.Length < 2)
+            if (args != null && args.Length>0)
             {
-                Usage();
-                return;
+                int port = args.Length >= 2 ? int.Parse(args[1]) : 0;
+                InitEndPoint(args[0], port);
             }
-            /*
-             * 
-             * 
-             * Usage:
-             *    node 127.0.0.1 8000
-             * 
-             * Starts the endpoint on IP address 127.0.0.1 port 8000.
-             *
-             * 
-             * 
-             */
-
-            _Ip = args[0];
-            _Port = Convert.ToInt32(args[1]);
-
-            _UdpEndpoint = new UdpEndpoint(_Ip, _Port);
-            _UdpEndpoint.EndpointDetected += EndpointDetected;
-            _UdpEndpoint.DatagramReceived += DatagramReceived;
-            _UdpEndpoint.Events.Started += NodeStarted;
-            _UdpEndpoint.Events.Stopped += NodeStopped;
-
-            _UdpEndpoint.Start();
             
+            Console.WriteLine("[" + (_udpEndpoint!= null ? _udpEndpoint.Socket.LocalEndPoint.ToString() : "") + " Command/? for help ]: ");
+            Console.WriteLine();
+
             while (true)
             {
-                Console.Write("[" + _Ip + ":" + _Port + " Command/? for help]: ");
-                string userInput = Console.ReadLine();
-                if (String.IsNullOrEmpty(userInput)) continue;
+                try {
+                    Console.Write("> ");
+                    string userInput = Console.ReadLine();
+                    if (String.IsNullOrEmpty(userInput)) continue;
 
-                if (userInput.Equals("?"))
-                {
-                    Menu();
-                }
-                else if (userInput.Equals("q"))
-                {
-                    break;
-                }
-                else if (userInput.Equals("cls"))
-                {
-                    Console.Clear();
-                }
-                else if (userInput.Equals("list"))
-                {
-                    List<string> recents = _UdpEndpoint.Endpoints;
-                    if (recents != null)
+                    if (userInput.Equals("?"))
                     {
-                        Console.WriteLine("Recent endpoints");
-                        foreach (string endpoint in recents) Console.WriteLine("  " + endpoint);
+                        Menu();
                     }
-                    else
+                    else if (userInput.Equals("q"))
                     {
-                        Console.WriteLine("None");
+                        Environment.Exit(0);
+                    }
+                    else if (userInput.Equals("cls"))
+                    {
+                        Console.Clear();
+                    }
+                    else if (userInput.StartsWith("bind"))
+                    {
+                        if (_udpEndpoint != null && !_udpEndpoint.Disposed) throw new Exception("Already bind");
+                        var split = userInput.Split(' ');
+                        string ip = split.Length < 2 ? "127.0.0.1" : split[1];
+                        int port = split.Length < 3 ? 0 : int.Parse(split[2]);
+                        InitEndPoint(ip, port);
+                        Console.WriteLine(_udpEndpoint.Socket.LocalEndPoint.ToString());
+                    }
+                    else if (userInput.Equals("list"))
+                    {
+                        List<EndPoint> recents = _udpEndpoint.EndPoints;
+                        if (recents != null)
+                        {
+                            Console.WriteLine("Recent endpoints");
+                            foreach (var endpoint in recents) Console.WriteLine("  " + endpoint.ToString());
+                        }
+                        else
+                        {
+                            Console.WriteLine("None");
+                        }
+                    }
+                    else if (userInput.Equals("start"))
+                    {
+                        _udpEndpoint.Start();
+                    }
+                    else if (userInput.Equals("stop"))
+                    {
+                        _udpEndpoint.Stop();
+                    }
+                    else if (userInput.Equals("dispose"))
+                    {
+                        _udpEndpoint.Dispose();
+                    }
+                    else if (userInput.Equals("status"))
+                    {
+                        if (_udpEndpoint == null)
+                        {
+                            Console.WriteLine("not bind");
+                        }
+                        else
+                        if (_udpEndpoint.Disposed)
+                        {
+                            Console.WriteLine("disposed");
+                        }
+                        else
+                        {
+                            Console.WriteLine(_udpEndpoint.Started ? "started" : "stopped");
+                        }
+                    }
+                    else if (userInput.StartsWith("echo"))
+                    {
+                        var split = userInput.Split(' ');
+                        var msg = split[1];
+                        _udpEndpoint.Send(_udpEndpoint.Socket.LocalEndPoint, msg);
+                    }
+                    else if (userInput.StartsWith("send"))
+                    {
+                        var split = userInput.Split(' ');
+
+                        var port = int.Parse(split[1]);
+                        var msg = split[2];
+                        _udpEndpoint.Send(_udpEndpoint.Socket.LocalEndPoint.ToString().Split(':')[0], port, msg);
+                    }
+                    else if (userInput.StartsWith("send2"))
+                    {
+                        var split = userInput.Split(' ');
+
+                        var ip = split[1];
+                        var port = int.Parse(split[2]);
+                        var msg = split[3];
+                        _udpEndpoint.Send(ip, port, msg);
+                    }
+                    else if (userInput.Length != 0)
+                    {
+                        Console.WriteLine("Unknown command!");
                     }
                 }
-                else if (userInput.Equals("start"))
+                catch (Exception ex)
                 {
-                    _UdpEndpoint.Start();
-                }
-                else if (userInput.Equals("stop"))
-                {
-                    _UdpEndpoint.Stop();
-                }
-                else
-                {
-                    string[] parts = userInput.Split(new char[] { ' ' }, 2);
-                    if (parts.Length != 2) continue;
-
-                    string msg = parts[1];
-                    string ipPort = parts[0];
-                    int colonIndex = parts[0].LastIndexOf(':');
-                    string ip = ipPort.Substring(0, colonIndex);
-                    int port = Convert.ToInt32(ipPort.Substring(colonIndex + 1));
-
-                    _UdpEndpoint.Send(ip, port, msg);
+                    Console.WriteLine("Error: "+ex.Message);
                 }
             }
 
             Console.ReadLine();
         }
 
-        static void Usage()
+        static void InitEndPoint(string ip, int port)
         {
-            Console.WriteLine("Usage:");
-            Console.WriteLine("> node 127.0.0.1 8000");
-            Console.WriteLine("Starts the endpoint on IP address 127.0.0.1 port 8000");
+            _udpEndpoint = new UdpEndpoint(ip, port);
+            _udpEndpoint.EndpointDetected += EndpointDetected;
+            _udpEndpoint.DatagramReceived += DatagramReceived;
+            _udpEndpoint.Events.Started += NodeStarted;
+            _udpEndpoint.Events.Stopped += NodeStopped;
+            Console.Title = "SimpleUdp " + _udpEndpoint.Socket.LocalEndPoint.ToString();
         }
 
         static void Menu()
         {
             Console.WriteLine("");
-            Console.WriteLine("Available commands");
-            Console.WriteLine("  q       quit");
-            Console.WriteLine("  ?       help, this menu");
-            Console.WriteLine("  cls     clear the screen");
-            Console.WriteLine("  list    list recent endpoints");
-            Console.WriteLine("  start   start the endpoint");
-            Console.WriteLine("  stop    stop the endpoint");
+            Console.WriteLine("Usage:");
+            Console.WriteLine("> node 127.0.0.1 8000");
+            Console.WriteLine("Starts the endpoint on IP address 127.0.0.1 port 8000");
             Console.WriteLine("");
-            Console.WriteLine("To send a message, use the form 'ip:port message', i.e.");
-            Console.WriteLine("127.0.0.1:8001 hello world!");
+            Console.WriteLine("Available commands");
+            Console.WriteLine("  q              quit");
+            Console.WriteLine("  ?              help, this menu");
+            Console.WriteLine("  cls            clear the screen");
+            Console.WriteLine("  bind           create socket and bind 127.0.0.1");
+            Console.WriteLine("  bind [ip]      create socket and bind an spicific ip");
+            Console.WriteLine("  bind [ip] [port]      create socket and bind an specifics ip and port number");
+            Console.WriteLine("  list           list recent endpoints");
+            Console.WriteLine("  start          start the endpoint");
+            Console.WriteLine("  stop           stop the endpoint");
+            Console.WriteLine("  dispose        close socket");
+            Console.WriteLine("  status         show endpoint listener status");
+            Console.WriteLine("  echo [msg]     send msg to yourself");
+            Console.WriteLine("  send [port] [msg]      send msg over the same ip");
+            Console.WriteLine("  send2 [ip] [port] [msg] send msg to cpecific endpoint");
             Console.WriteLine("");
         }
 
-        static void EndpointDetected(object sender, EndpointMetadata md)
+        static void EndpointDetected(object sender, EndPoint ep)
         {
-            Console.WriteLine("Endpoint detected: " + md.Ip + ":" + md.Port);
+            Console.WriteLine("Endpoint detected: " + ep.ToString());
         }
 
         static void DatagramReceived(object sender, Datagram dg)
         {
-            Console.WriteLine("[" + dg.Ip + ":" + dg.Port + "]: " + Encoding.UTF8.GetString(dg.Data));
+            Console.WriteLine("[" + dg.endPoint.ToString() + "]: " + Encoding.UTF8.GetString(dg.data));
         }
 
         private static void NodeStarted(object sender, EventArgs e)
